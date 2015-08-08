0f539f28747eb03aed5b67e5807b5b41d85e9fa8
JelenaCosic <jcjelenacosic@gmail.com>
2015-08-07 22:28:01 -0400

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,15 @@
+data <- read.table("c:/data/household_power_consumption.txt", header=T, sep=";")
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+subset <- data[(data$Date=="2007-02-01") | (data$Date=="2007-02-02"),]
+subset$Global_active_power <- as.numeric(as.character(subset$Global_active_power))
+subset <- transform(subset, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+subset$Sub_metering_1 <- as.numeric(as.character(subset$Sub_metering_1))
+subset$Sub_metering_2 <- as.numeric(as.character(subset$Sub_metering_2))
+subset$Sub_metering_3 <- as.numeric(as.character(subset$Sub_metering_3))
+
+plot(subset$timestamp,subset$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(subset$timestamp,subset$Sub_metering_2,col="red")
+lines(subset$timestamp,subset$Sub_metering_3,col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.copy(png, file="plot3.png", width=480, height=480)
+dev.off()
